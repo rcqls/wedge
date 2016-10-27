d1a1b3fbe4279dc4354ade7940ff8abfb1a731b7
R Cqls <rdrouilh@gmail.fr>
2016-10-27 06:44:58 +0200

    add minimal README

--- demo/wedgePerf.R
+++ demo/wedgePerf.R
@@ -1,6 +1,6 @@
 require(wedge)
 
-wedge.perf <- function(nbs.exp=c(10^6,10^7,10^8),tests=c("R","Rcpp","RcppParallel","RcppCores"),seed=10,N=3,nbs.threads=c(4,6,8),nbs.cores=c(4,6,8)) {
+wedge.perf <- function(nbs.exp=c(10^6,10^7,10^8),tests=c("R","Rcpp","RcppParallel"),seed=10,N=3,nbs.threads=c(4,6,8),nbs.cores=c(4,6,8)) {
   set.seed(seed)
   for(nb.exp in nbs.exp) {
     a1 <- 10*runif(nb.exp)^2
@@ -24,10 +24,10 @@ wedge.perf <- function(nbs.exp=c(10^6,10^7,10^8),tests=c("R","Rcpp","RcppParalle
       for(nb.cores in nbs.cores) {
         cat("Rcpp",nb.cores,"cores with nb.exp=",nb.exp,"\n")
         print(system.time({
-          b <- rep(nb.exp %/% nb.cores,nb.cores) + c(rep(1,r <- nb.exp %% nb.cores),rep(0,nb.cores - r))
-          chunksSup <- cumsum(b)
+          size <- rep(nb.exp %/% nb.cores,nb.cores) + c(rep(1,r <- nb.exp %% nb.cores),rep(0,nb.cores - r))
+          chunksSup <- cumsum(size)
           chunksInf <- 1 + c(0,chunksSup[-length(chunksSup)])
-          wpRcppCore<-unlist(mclapply(1:nb.cores,function(i) wedge(a1[(chunksInf[i]:chunksSup[i])->ind],b1[ind],a2[ind],b2[ind],size,tau,type="Rcpp"),mc.cores=nb.cores))
+          wpRcppCore<-unlist(mclapply(1:nb.cores,function(i) wedge(a1[(chunksInf[i]:chunksSup[i])->ind],b1[ind],a2[ind],b2[ind],size[i],tau,type="Rcpp"),mc.cores=nb.cores))
         }))
         print(summary(wpRcppCore))
       }
